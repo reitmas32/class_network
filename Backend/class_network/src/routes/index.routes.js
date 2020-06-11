cac09da257f6e39683a344ee5a09ccce8f8f4185
Reitmas32 <rafa.zamora.rals@gmail.com>
2020-06-11 02:24:31 -0500

    Se añadio una ruta index para probar en diferentes PCs la coneccion al server

---
+++ Backend/class_network/src/routes/index.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.send("Coneccion exitosa a la Servidor");
+});
+
+export default router;
--- Backend/class_network/src/server.js
+++ Backend/class_network/src/server.js
@@ -2,6 +2,9 @@ import express from "express";
 
 const app = express();
 
+//TODO:Eliminar suando este en produccion
+import Index from './routes/index.routes';
+
 import Sing_up from './routes/user/sing_up.routes';
 import Login from './routes/user/login.routes';
 
@@ -12,6 +15,9 @@ app.set("port", process.env.PORT || 3000);
 app.use(express.json());
 
 //Routes
+//TODO:Eliminar suando este en produccion
+app.use('/index', Index);
+
 app.use('/api/user/singup', Sing_up);
 app.use('/api/user/login', Login);
 
