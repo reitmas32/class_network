fe5fcbad0191bd8e3b44a5a3e0d827144bb812af
Reitmas32 <rafa.zamora.rals@gmail.com>
2020-06-11 12:04:54 -0500

    Enum dias

---
+++ Frontend/class_network/lib/util/days_enum.dart
@@ -0,0 +1,9 @@
+class Days{
+  static const int monday = 0;
+  static const int tuesday = 1;
+  static const int wednesday = 2;
+  static const int thursday = 3;
+  static const int friday = 4;
+  static const int saturday = 5;
+  static const int sunday = 6;
+}
