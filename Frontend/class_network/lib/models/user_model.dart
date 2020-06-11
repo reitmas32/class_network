ea101e81e5458b829e3a10508ea6dc441e1547cb
Reitmas32 <rafa.zamora.rals@gmail.com>
2020-06-11 11:21:01 -0500

    Se añadio el campo _id al user

--- Frontend/class_network/lib/models/user_model.dart
+++ Frontend/class_network/lib/models/user_model.dart
@@ -2,6 +2,7 @@ class User {
   String userName;
   String email;
   String password;
+  String _id;
 
   User({this.userName, this.password, this.email});
 
@@ -9,13 +10,15 @@ class User {
     this.userName = json['userName'];
     this.password = json['password'];
     this.email = json['email'];
+    this._id = json[_id];
   }
 
   Map<String, dynamic> toJSON() {
     return {
       'userName': this.userName,
       'password': this.password,
-      'email': this.email
+      'email': this.email,
+      '_id': this._id,
     };
   }
 }
