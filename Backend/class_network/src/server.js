17e5e84ce50fca2ad57fa07fa770f6c9ef76b615
Reitmas32 <rafa.zamora.rals@gmail.com>
2020-06-11 02:19:28 -0500

    Se añadio las rutas SingUp y Login

--- Backend/class_network/src/server.js
+++ Backend/class_network/src/server.js
@@ -2,8 +2,17 @@ import express from "express";
 
 const app = express();
 
+import Sing_up from './routes/user/sing_up.routes';
+import Login from './routes/user/login.routes';
+
 //Settings
 app.set("port", process.env.PORT || 3000);
 
+// Meddleweres
+app.use(express.json());
+
+//Routes
+app.use('/api/user/singup', Sing_up);
+app.use('/api/user/login', Login);
 
 export default app;
