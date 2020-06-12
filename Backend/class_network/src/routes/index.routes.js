6e309eee2fd642eef4d709afe01819fe341dd2cc
Reitmas32 <rafa.zamora.rals@gmail.com>
2020-06-12 18:23:15 -0500

    se agrego un comentario para mostrar en la consola

--- Backend/class_network/src/routes/index.routes.js
+++ Backend/class_network/src/routes/index.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get('/', (req, res) => {
+    console.log("Se conecto alguien " + req.ip);
     res.send("Coneccion exitosa a la Servidor");
 });
 
