ad3d6becc5d123902a6f078548770a028cdc31a4
reitmas32 <rafa.zamora.rals@gmail.com>
2020-08-11 04:02:12 -0500

    Se inicio con el Soporte de lenguajes

---
+++ Frontend/class_network/lib/util/languge.dart
@@ -0,0 +1,61 @@
+class Strings {
+  Map<String, String> strings;
+}
+
+class StringsES extends Strings {
+  StringsES() {
+    this.strings = {
+      //Login
+      'UserNameString': 'Nombre de Usuario',
+      'Password': 'Contraseña',
+      'Login': 'Inicia Sesión',
+      'SignIn': 'Registrarse',
+      'Remember': 'Recordarme',
+      //SignIn
+      'Cancel': 'Cancelar',
+      'Condition': 'Acepto los Terminos y condiciones',
+      'Acept': 'Acepto',
+      'Email': 'Correo',
+      'IDoNotConditions': 'No acepto las Condiciones',
+      'Understand': 'Entiendo',
+      'Subjects': 'Materias'
+    };
+  }
+}
+
+class StringsEN extends Strings {
+  StringsEN() {
+    this.strings = {
+      //Login
+      'UserNameString': 'User Name',
+      'Password': 'Password',
+      'Login': 'Login',
+      'SignIn': 'SignIn',
+      'Remember': 'Remember',
+      //SignIn
+      'Cancel': 'Cancel',
+      'Condition': 'I accept the conditions',
+      'Acept': 'Acept',
+      'Email': 'Email',
+      'IDoNotConditions': 'I do not accept the conditions',
+      'Understand': 'Understand',
+      'Subjects': 'Subjects'
+    };
+  }
+}
+
+class ControllerStrings {
+  static Strings dataStrings;
+  static String getStringValue(String key) {
+    return ControllerStrings.dataStrings.strings[key];
+  }
+
+  static void setLanguage(String language) {
+    if (language == 'es') {
+      ControllerStrings.dataStrings = StringsES();
+    }
+    if (language == 'en') {
+      ControllerStrings.dataStrings = StringsEN();
+    }
+  }
+}
