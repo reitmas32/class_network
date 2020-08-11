514d0998e1e23cada4a872ff148de6f82644a0e4
reitmas32 <rafa.zamora.rals@gmail.com>
2020-08-11 04:02:56 -0500

    Se refactorizo el widget

--- Frontend/class_network/lib/widgets/subject_card.dart
+++ Frontend/class_network/lib/widgets/subject_card.dart
@@ -3,109 +3,113 @@ import 'dart:math' as math;
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
+import 'package:class_network/widgets/button_card_cn.dart';
+
 class SubjectCard extends StatefulWidget {
   @override
   _SubjectCardState createState() => _SubjectCardState();
 }
 
 class _SubjectCardState extends State<SubjectCard> {
+  ShapeBorder shapeBorder;
+  Color colorBack;
+
+  void initState() {
+    shapeBorder = RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(
+        Radius.circular(15.0),
+      ),
+    );
+    colorBack = Colors.red;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(12.0),
+      padding: const EdgeInsets.all(8.0),
       child: Container(
         height: MediaQuery.of(context).size.height / 4,
         child: Stack(
           children: [
-            Material(
-              color: Colors.red,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(15.0),
-                ),
-              ),
-              child: Row(
-                children: [
-                  Column(
-                    children: [
-                      Text(
-                        'Nombre Materia',
-                        style: TextStyle(
-                            fontSize: 30.0,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        'Nombre Profesor',
-                        style: TextStyle(
-                          fontSize: 27.0,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                  ),
-                  Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Transform.rotate(
-                          child: Material(
-                            shape: CircleBorder(),
-                            color: Colors.red,
-                            child: InkWell(
-                              child: Padding(
-                                padding: const EdgeInsets.all(4.0),
-                                child: Icon(
-                                  Icons.label_important,
-                                  size: 60.0,
-                                  color: Colors.amber,
-                                ),
-                              ),
-                              onTap: () {
-                                print("2");
-                              },
-                              customBorder: CircleBorder(),
-                            ),
-                          ),
-                          angle: 3 * math.pi / 2,
-                        ),
-                      ),
-                    ],
-                    mainAxisAlignment: MainAxisAlignment.start,
-                  )
-                ],
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-              ),
-            ),
-            Align(
-              child: Padding(
-                padding: const EdgeInsets.only(right: 60.0, bottom: 8.0),
-                child: Material(
-                  shape: CircleBorder(),
-                  color: Colors.red,
-                  child: InkWell(
-                    child: Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Icon(
-                        Icons.edit,
-                        size: 60.0,
-                        color: Colors.amber,
-                      ),
-                    ),
-                    onTap: () {
-                      print("1");
-                    },
-                    customBorder: CircleBorder(),
-                  ),
-                ),
-              ),
-              alignment: Alignment.bottomRight,
-            ),
+            _buildBackCard(),
+            _buildButtonMarkerSubject(),
+            _buildButtonEditSubject(),
           ],
         ),
       ),
+    );
+  }
+
+  Material _buildBackCard() {
+    return Material(
+      color: this.colorBack,
+      shape: this.shapeBorder,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            _buildTextNameSubject(),
+            _buildTextTeacherSubject(),
+          ],
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+        ),
+      ),
+    );
+  }
+
+  Text _buildTextTeacherSubject() {
+    return Text(
+      'Nombre Profesor',
+      style: TextStyle(
+        fontSize: 27.0,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  Text _buildTextNameSubject() {
+    return Text(
+      'Nombre Materia',
+      style: TextStyle(
+        fontSize: 30.0,
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  ButtonCard_CN _buildButtonMarkerSubject() {
+    return ButtonCard_CN(
+      padding: EdgeInsets.only(right: 30.0, top: 8.0),
+      shapeBorder: CircleBorder(),
+      colorBack: Colors.red,
+      colorButton: Colors.amber,
+      alignment: Alignment.topRight,
+      size: 60.0,
+      angle: 3 * math.pi / 2,
+      iconData: Icons.label_important,
+      elevation: 5.0,
+      callback: () {
+        print("2");
+      },
+    );
+  }
+
+  ButtonCard_CN _buildButtonEditSubject() {
+    return ButtonCard_CN(
+      padding: EdgeInsets.only(right: 60.0, bottom: 8.0),
+      shapeBorder: CircleBorder(),
+      colorBack: Colors.red,
+      colorButton: Colors.amber,
+      alignment: Alignment.bottomRight,
+      size: 60.0,
+      iconData: Icons.edit,
+      elevation: 5.0,
+      callback: () {
+        print("1");
+      },
     );
   }
 }
