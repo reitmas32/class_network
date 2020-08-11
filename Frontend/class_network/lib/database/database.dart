ccaa3735379f7bcbe49372d38a8a067af7f463bc
reitmas32 <rafa.zamora.rals@gmail.com>
2020-08-11 04:00:16 -0500

    Se añadio la funcion de login

--- Frontend/class_network/lib/database/database.dart
+++ Frontend/class_network/lib/database/database.dart
@@ -1,9 +1,26 @@
 import 'package:class_network/models/user_model.dart';
 
-class DB{
-  static Future<User> createUser() async {
+class DB {
+  static List<User> users = List();
+  //TODO:Hacer la lista de materias del usuario
+  //TODO:Hacer que los datos se guarden
+  static Future<User> singIn(String name, String password, String email) async {
     Future.delayed(Duration(seconds: 1));
-    User user = User(userName: 'rafa', password: 'tereza1234', email: 'rafa.zamora.rals@gmail.com');
+    User user = User(userName: name, password: password, email: email);
+    users.add(user);
+    print(users);
     return user;
+  }
+
+  static Future<User> login(String name, String password) async {
+    Future.delayed(Duration(seconds: 1));
+    for (User user in users) {
+      if (user.userName == name && user.password == password) {
+        //TODO:Regresar las materias del usuario
+        print('se inicio sesion');
+        return user;
+      }
+    }
+    return null;
   }
 }
